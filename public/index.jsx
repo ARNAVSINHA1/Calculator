74bde942cc34e3a31988e4d1df751a45dd27568e
ARNAVSINHA1 <arnavsinha.er@gmail.com>
2024-02-05 12:57:39 +0530

    slight updates

--- public/index.jsx
+++ public/index.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'src/App.jsx';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
